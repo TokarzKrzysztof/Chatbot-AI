24cc86ab96b12ca6ef0d456f8a9e8e219bcabd7c
Krzysztof Tokarz <53493825+TokarzKrzysztof@users.noreply.github.com>
2025-07-14 13:38:09 +0200

    backend init

---
+++ backend/Controllers/ChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+	[ApiController]
+	[Route("[controller]/[action]")]
+	public class ChatController: ControllerBase
+	{
+		public async Task<IActionResult> GetChatHistory()
+		{
+			return Ok();
+		}
+	}
+}
